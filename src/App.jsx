a8adfe1b1387d3f7df8505caf034cb10a92eca41
jimena <herrerajimena01@gmail.com>
2024-02-17 19:04:41 -0300

    se crearon navbar, footer y lista de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Navbarr from "./components/Navbar/Navbar"
+import Footer from "./components/Navbar/Footer/Footer"
+import Tareas from "./components/Navbar/Tareas/Tareas"
 function App() {
-  const [count, setCount] = useState(0)
-  const [nombre , setNombre]=  useState("");
+  
 //toda la logica del componente vive aqui antes del return
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() =>{ setCount((count) => count + 1)
-        setNombre((nombre) => nombre + "A-")
-        } }>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <h1>Hola Mundo React</h1>
-      <p>Valor del estado Nombre: {nombre} </p>
+      <Navbarr/>
+      <Tareas></Tareas>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Navbar/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="container-fluid bg-dark text-white text-center p-5">
+            <p>Todos los derechos reservados</p>
+        </div>
+    );
+};
+
+export default Footer;
